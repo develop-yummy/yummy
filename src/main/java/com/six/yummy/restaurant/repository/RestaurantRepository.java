aed933871b42faa55766e6f7c867fb5f2f171156
tangpoo <lhinav@naver.com>
2024-02-24 17:53:33 +0900

    :sparkles: 식당 등록

--- src/main/java/com/six/yummy/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/six/yummy/restaurant/repository/RestaurantRepository.java
@@ -1,4 +1,7 @@
 package com.six.yummy.restaurant.repository;
 
-public interface RestaurantRepository {
+import com.six.yummy.restaurant.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 }
--- src/main/java/com/six/yummy/restaurant/responsedto/RestaurantResponse.java
+++ src/main/java/com/six/yummy/restaurant/responsedto/RestaurantResponse.java
@@ -1,4 +1,23 @@
 package com.six.yummy.restaurant.responsedto;
 
+import com.six.yummy.restaurant.entity.Restaurant;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class RestaurantResponse {
+    Long restaurantId;
+    String restaurantName;
+    String address;
+    String content;
+    String category;
+
+    @Builder
+    public RestaurantResponse(Long restaurantId, String restaurantName, String address, String content, String category) {
+        this.restaurantId = restaurantId;
+        this.restaurantName = restaurantName;
+        this.address = address;
+        this.content = content;
+        this.category = category;
+    }
 }
