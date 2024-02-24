dc3d7824bd3bc266ecdf421cf7d650e02ae7f5e4
si eun <tldms123455@naver.com>
2024-02-25 02:52:26 +0900

    :fire: config -> util로 교체

--- src/main/java/com/six/yummy/global/util/PasswordEncoderUtil.java
+++ src/main/java/com/six/yummy/global/util/PasswordEncoderUtil.java
@@ -1,4 +1,4 @@
-package com.six.yummy.global.config;
+package com.six.yummy.global.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordConfig {
+public class PasswordEncoderUtil {
     @Bean
     public PasswordEncoder passwordEncoder () {
         return new BCryptPasswordEncoder();
