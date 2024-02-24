d148eae2aba83f2ada816d2aa871d8be3d736ffd
tangpoo <lhinav@naver.com>
2024-02-24 20:29:59 +0900

    :recycle: Restaurant 기본 생성자 접근 제어자 변경

--- src/main/java/com/six/yummy/restaurant/entity/Restaurant.java
+++ src/main/java/com/six/yummy/restaurant/entity/Restaurant.java
@@ -1,17 +1,17 @@
 package com.six.yummy.restaurant.entity;
 
-import com.six.yummy.restaurant.requestdto.RestaurantRequest;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Restaurant {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,8 +23,10 @@ public class Restaurant {
     @Column(nullable = false)
     String address;
 
+    @Column
     String content;
 
+    @Column
     String category;
 
     private Restaurant(String restaurantName, String address, String content, String category) {
