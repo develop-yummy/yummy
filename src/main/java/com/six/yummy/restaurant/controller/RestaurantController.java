78ceeaab1583bad7c997079966bbbc04f63de285
tangpoo <lhinav@naver.com>
2024-02-25 14:40:39 +0900

    :recycle: Restaurant 유저, private 추가

--- src/main/java/com/six/yummy/restaurant/controller/RestaurantController.java
+++ src/main/java/com/six/yummy/restaurant/controller/RestaurantController.java
@@ -3,10 +3,12 @@ package com.six.yummy.restaurant.controller;
 import com.six.yummy.restaurant.requestdto.RestaurantRequest;
 import com.six.yummy.restaurant.responsedto.RestaurantResponse;
 import com.six.yummy.restaurant.service.RestaurantService;
+import com.six.yummy.user.lmpl.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +23,10 @@ public class RestaurantController {
 
     @PostMapping
     public ResponseEntity<RestaurantResponse> saveRestaurant(
-        @Valid @RequestBody RestaurantRequest restaurantRequest
+        @Valid @RequestBody RestaurantRequest restaurantRequest,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
     ){
-        RestaurantResponse restaurantResponse = restaurantService.saveRestaurant(restaurantRequest);
+        RestaurantResponse restaurantResponse = restaurantService.saveRestaurant(restaurantRequest, userDetails.getUser());
 
         return new ResponseEntity<>(restaurantResponse, HttpStatus.CREATED);
     }
--- src/main/java/com/six/yummy/restaurant/entity/Restaurant.java
+++ src/main/java/com/six/yummy/restaurant/entity/Restaurant.java
@@ -1,10 +1,15 @@
 package com.six.yummy.restaurant.entity;
 
+import com.six.yummy.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,30 +20,35 @@ import lombok.NoArgsConstructor;
 public class Restaurant {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long restaurantId;
+    private Long restaurantId;
 
     @Column(nullable = false)
-    String restaurantName;
+    private String restaurantName;
 
     @Column(nullable = false)
-    String address;
+    private String address;
 
     @Column
-    String content;
+    private String content;
 
     @Column
-    String category;
+    private String category;
 
-    private Restaurant(String restaurantName, String address, String content, String category) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Restaurant(String restaurantName, String address, String content, String category, User user) {
         this.restaurantName = restaurantName;
         this.address = address;
         this.content = content;
         this.category = category;
+        this.user = user;
     }
 
     //생성 메서드
-    public static Restaurant createRestaurant(String restaurantName, String address, String content, String category){
+    public static Restaurant createRestaurant(String restaurantName, String address, String content, String category, User user){
 
-        return new Restaurant(restaurantName, address, content, category);
+        return new Restaurant(restaurantName, address, content, category, user);
     }
 }
