ecf33471a16542430c705ed79d6c636b35cf3889
HEEEUN9812 <heeeun9812@gmail.com>
2024-02-27 22:04:10 +0900

    :recycle: Exception 수정

--- src/main/java/com/six/yummy/global/exception/NotFoundCartItemException.java
+++ src/main/java/com/six/yummy/global/exception/NotFoundCartItemException.java
@@ -1,6 +1,6 @@
 package com.six.yummy.global.exception;
 
-public class NotFoundCartItemException extends IllegalArgumentException{
+public class NotFoundCartItemException extends IllegalArgumentException {
 
     public NotFoundCartItemException() {
         super("장바구니 내 해당 상품을 찾을 수 없습니다.");
--- src/main/java/com/six/yummy/global/exception/ValidateUserException.java
+++ src/main/java/com/six/yummy/global/exception/ValidateUserException.java
@@ -1,6 +1,7 @@
 package com.six.yummy.global.exception;
 
 public class ValidateUserException extends RuntimeException {
+
     public ValidateUserException() {
         super("해당 유저에 대한 권한이 없습니다.");
     }
