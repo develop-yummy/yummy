9ef367ffe47aa438b89d2337d38b517c44d6d234
si eun <tldms123455@naver.com>
2024-02-27 16:03:33 +0900

    Changes by heeeun

--- src/main/java/com/six/yummy/global/exception/ExceptionController.java
+++ src/main/java/com/six/yummy/global/exception/ExceptionController.java
@@ -1,8 +1,48 @@
 package com.six.yummy.global.exception;
 
+import com.six.yummy.global.exception.dto.ExceptionDto;
+import javax.xml.crypto.Data;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler({NotFoundUserException.class, NotFoundMenuException.class,
+        NotFoundReviewException.class, NotFoundRestaurantException.class})
+    public ResponseEntity<ExceptionDto> NotFoundException(NotFoundException e) {
+        return ResponseEntity.badRequest()
+            .body(new ExceptionDto(e.getMessage(), HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler({ValidateUserException.class, ValidateReviewException.class})
+    public ResponseEntity<ExceptionDto> ValidateException(Exception e) {
+        return ResponseEntity.badRequest()
+            .body(new ExceptionDto(e.getMessage(), HttpStatus.FORBIDDEN));
+    }
+
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ExceptionDto> NullPointerException(Exception e) {
+        return ResponseEntity.badRequest()
+            .body(new ExceptionDto(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    @ExceptionHandler({DataIntegrityViolationReviewException.class, DataIntegrityViolationLikeException.class})
+    public ResponseEntity<ExceptionDto> DataIntegrityViolationException(
+        Exception e) {
+        return ResponseEntity.badRequest()
+            .body(new ExceptionDto(e.getMessage(),HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(IllegalArgumentFollowException.class)
+    public ResponseEntity<ExceptionDto> IllegalArgumentException(Exception e) {
+        return ResponseEntity.badRequest()
+            .body(new ExceptionDto(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
+    }
 
 }
