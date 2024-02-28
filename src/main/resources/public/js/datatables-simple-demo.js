3fb77d55ae1372805ca66efbe97b51aa85c75307
SerenityZenDev <tjrqo2000@gmail.com>
2024-02-28 17:52:39 +0900

    :recycle: 프론트 메인페이지 테이블 동적데이터 연결 후 기타기능 적용

--- src/main/resources/public/js/datatables-simple-demo.js
+++ src/main/resources/public/js/datatables-simple-demo.js
@@ -1,52 +1,40 @@
 $(document).ready(function (){
-    console.log("문서 준비 완료")
+    console.log("문서 준비 완료");
     $.ajax({
-        url: "http://localhost:8080/backoffice/restaurants/sales", // 스프링부트 백엔드 URL로 변경해야 합니다.
+        url: "http://localhost:8080/backoffice/restaurants/sales",
         type: "GET",
         dataType: "json",
         beforeSend: function(xhr) {
-            // 세션 스토리지에서 토큰 가져오기
             var token = sessionStorage.getItem("Authorization");
             if (token) {
-                // 요청 헤더에 토큰 추가
                 xhr.setRequestHeader("Authorization", token);
                 console.log("토큰 준비 완료");
             }
         },
         success: function(data) {
-            // 데이터 로딩이 성공했을 때 실행될 함수
             console.log("데이터 로딩 완료");
             var tbody = $("#datatablesSimple tbody");
-            tbody.empty(); // 기존 테이블 내용을 비웁니다.
+            tbody.empty();
 
-            // 받아온 데이터를 사용하여 테이블 행을 동적으로 생성합니다.
             data.forEach(function(item) {
-                var row = "<tr>" +
-                    "<td>" + item.restaurantName + "</td>" +
-                    "<td>" + item.address + "</td>" +
-                    "<td>" + item.content + "</td>" +
-                    "<td>" + item.category + "</td>" +
-                    "<td>$" + item.sales + "</td>" +
-                    "</tr>";
+                var row = `<tr>
+                    <td>${item.restaurantName}</td>
+                    <td>${item.address}</td>
+                    <td>${item.content}</td>
+                    <td>${item.category}</td>
+                    <td>${item.sales.toLocaleString('en-US')}</td>
+                    </tr>`;
                 tbody.append(row);
             });
 
+            // 기존의 Simple DataTables 인스턴스 제거 및 새로 초기화
+            if (datatablesSimple.DataTable) {
+                datatablesSimple.DataTable.destroy();
+            }
+            new simpleDatatables.DataTable(datatablesSimple);
         },
         error: function(xhr, status, error) {
-            // 데이터 로딩에 실패했을 때 실행될 함수
             console.error("Data loading failed: ", error);
         }
     });
-})
-
-
-
-window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
-
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
-    }
 });
