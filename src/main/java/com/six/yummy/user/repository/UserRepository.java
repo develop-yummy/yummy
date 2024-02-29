60ea69c11de52d0a08a3cd250deb240e8345cd86
si eun <tldms123455@naver.com>
2024-02-29 10:00:35 +0900

    :recycle: refactor : signup method refactor

--- src/main/java/com/six/yummy/user/repository/UserRepository.java
+++ src/main/java/com/six/yummy/user/repository/UserRepository.java
@@ -7,11 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String username);
-
+//    Optional<User> findByUsername(String username);
+//
     Optional<User> findByEmail(String email);
+//
+//    Optional<User> findByPhoneNumber(String phoneNumber); // 이렇게 Optional로 반환되는게 낭비
 
-    Optional<User> findByPhoneNumber(String phoneNumber);
+    boolean existByUsername();
+
+    boolean existByEmail();
+
+    boolean existByPhoneNumber();
 
 //    @Query("select u from User u where u.email = ?1") //jpql 만들어줘
 //    Optional<User> findByEmailAndPhoneNumber(String email, String phoneNumber);
